bb24a017bb61fe2c26a79e06efa80ea64d5425e7
Artur-WebDev <web.artur.work@gmail.com>
2022-11-21 15:40:13 +0300

    first commit

---
+++ setting_theme_sublimetext.js
@@ -0,0 +1,34 @@
+{
+	"variables":
+	{
+
+	},
+	"globals":
+	{
+        "background": "#22262A",
+	},
+	"rules":
+	[
+		{
+            "name": "Comment",
+            "scope": "comment",
+            "foreground": "#72AF7D"
+        },
+        {
+            "name": "Number",
+            "scope": "constant.numeric",
+            "foreground": "#B4CEA6"
+        },
+        {
+            "name": "Function name",
+            "scope": "entity.name.function, keyword.other.name-of-parameter.objc",
+            "foreground": "#29BE94"
+        },
+        {
+            "name": "Function parameter",
+            "scope": "variable.parameter",
+            "foreground": "#E1C99A"
+        },
+ 
+	]
+}
